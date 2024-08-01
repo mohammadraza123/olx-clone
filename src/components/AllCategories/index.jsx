06528e56bcc412ee9e48f7627f92c0d2a9903ba8
Muhammad Raza <mohammadrazaarain10@gmail.com>
2024-08-01 23:19:51 +0500

    update UI

--- src/components/AllCategories/index.jsx
+++ src/components/AllCategories/index.jsx
@@ -7,18 +7,20 @@ export default function AllCategories() {
       <div className="pt-3 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <h1 className="font-bold pt-4 pb-4 text-2xl">All categories</h1>
         <div
-          className="flex md:grid md:grid-cols-9 overflow-x-auto md:overflow-visible space-x-4 md:space-x-0 gap-6 items-start"
+          className="flex md:grid md:grid-cols-9 overflow-x-auto md:overflow-visible space-x-4 md:space-x-0 md:gap-6 items-start"
           style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
         >
           {allCategoriesWithImages.map((item, index) => (
             <div
               key={index}
-              className="flex-shrink-0 md:flex-shrink flex flex-col items-center"
+              className="flex-shrink-0 md:flex-shrink flex flex-col items-center w-20"
             >
-              <div className="w-24">
+              <div className="w-14 md:w-24">
                 <img src={item.icon} className="w-full" alt={item.title} />
               </div>
-              <h2 className="text-center md:font-semibold">{item.title}</h2>
+              <h2 className="text-center text-[13px] md:text-[16px] md:font-semibold">
+                {item.title}
+              </h2>
             </div>
           ))}
         </div>
