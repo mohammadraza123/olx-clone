81d27a34b045ba5eed78053200c7aebb785b8472
Muhammad Raza <mohammadrazaarain10@gmail.com>
2024-09-04 23:59:10 +0500

    refactor

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -62,31 +62,41 @@ const Section = ({ title, links, navigate }) => (
 function Footer() {
   const navigate = useNavigate();
   return (
-    <>
-      <div className="mx-auto mt-8 sm:px-6 lg:px-8 block md:flex items-center justify-center gap-3 bg-[#f8faf9]">
-        <img src={footerBanner} className=" object-contain" />
-        <div className="p-6 text-center md:text-start">
-          <h1 className="text-4xl font-medium">TRY THE OLX APP</h1>
-          <h3 className="text-xl">
-            Buy, sell and find just about anything using the app on your mobile.
-          </h3>
-        </div>
+    <div className="relative min-h-screen">
+      {/* Your main content will go here */}
+      <div className=" sm:px-6 lg:px-8">
+        {/* Main content of the page */}
       </div>
-      <footer className="bg-[#ebeeef] border-t text-red py-10">
-        <div className="container mx-auto max-w-7xl px-2 pt-3 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-            {sections.map((section, index) => (
-              <Section
-                key={index}
-                title={section.title}
-                links={section.links}
-                navigate={navigate}
-              />
-            ))}
+
+      {/* Footer and Banner Section */}
+      <div className="absolute inset-x-0 bottom-0">
+        <div className="bg-[#f8faf9]">
+          <div className="flex items-center justify-center gap-3 mx-auto sm:px-6 lg:px-8">
+            <img src={footerBanner} className="object-contain" alt="Footer Banner" />
+            <div className="p-6 text-center md:text-start">
+              <h1 className="text-4xl font-medium">TRY THE OLX APP</h1>
+              <h3 className="text-xl">
+                Buy, sell and find just about anything using the app on your mobile.
+              </h3>
+            </div>
           </div>
         </div>
-      </footer>
-    </>
+        <footer className="bg-[#ebeeef] border-t text-red py-10">
+          <div className="container mx-auto max-w-7xl px-2 pt-3 sm:px-6 lg:px-8">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+              {sections.map((section, index) => (
+                <Section
+                  key={index}
+                  title={section.title}
+                  links={section.links}
+                  navigate={navigate}
+                />
+              ))}
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
   );
 }
 
